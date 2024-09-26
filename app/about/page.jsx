532cef14e6cd36e5aa6c01e699346daa75c50f1f
Muhammad Adnan Fareed <142770447+adnan32725@users.noreply.github.com>
2024-09-26 03:42:54 -0700

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -5,6 +5,7 @@ export default function About() {
           <div className="container mx-auto px-4 text-center">
             <h2 className="text-3xl font-bold mb-4">About Me</h2>
             <p className="text-lg max-w-2xl mx-auto">
+                
               I'm a passionate Full Stack Developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I'm focused on building responsive web applications with the latest technologies.
             </p>
           </div>
