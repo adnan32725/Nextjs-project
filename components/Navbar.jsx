3f0042bc33dd177505dec1820cc1e177ea5a1a32
Muhammad Adnan Fareed <142770447+adnan32725@users.noreply.github.com>
2024-09-29 01:26:32 -0700

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,6 @@ import { FiMenu } from "react-icons/fi";
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    
     <nav className="bg-gray-700 p-4">
       <div className="container mx-auto flex justify-between items-center">
         <div className="text-white text-2xl font-bold">MyWebsite</div>
